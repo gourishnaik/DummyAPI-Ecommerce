dee966b6806d3ce836bb9016d212895e7c2798ba
gourish naik <86231470+gourishnaik@users.noreply.github.com>
2023-06-15 17:24:18 +0530

    Update header.component.ts

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/shared/api.service';
+import { product } from '../product-view/productmodal';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,20 @@ import { ApiService } from 'src/app/shared/api.service';
 })
 export class HeaderComponent implements OnInit{
   public cartitems:number=0;
+  showproduct: any = [];
   constructor(private api:ApiService){}
   ngOnInit(): void {
   this.api.products().subscribe(res=>{
     this.cartitems = res.length;
+    this.showproduct = res;
   })
+  }
+
+  emptycart(){
+    this.api.removeallitems();
+  }
+  deleteitem(item: product) {
+    this.api.removecartitem(item)
   }
 
 }
