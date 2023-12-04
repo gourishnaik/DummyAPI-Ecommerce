ddb437ab8f6019fc4542700bcfed9a46bcd910b6
gourish naik <86231470+gourishnaik@users.noreply.github.com>
2023-12-04 15:38:21 +0530

    Update card-order.component.ts

--- src/app/component/card-order/card-order.component.ts
+++ src/app/component/card-order/card-order.component.ts
@@ -7,16 +7,18 @@ import { ApiService } from 'src/app/shared/api.service';
   templateUrl: './card-order.component.html',
   styleUrls: ['./card-order.component.css']
 })
-export class CardOrderComponent implements OnInit{
-  constructor(private router :Router,private api:ApiService){}
-ngOnInit(): void {
-  setTimeout(() => {
-    this.router.navigate(["/"]);
-    this.api.removeallitems();
-  }, 4000);
+export class CardOrderComponent implements OnInit {
+  userdata: any;
+  username: any;
+  constructor(private router: Router, private api: ApiService) { }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate(["/"]);
+      this.api.removeallitems();
+    }, 4000);
     //getting item from localstorage
-      let localdata = localStorage.getItem('ecomdata')
-      this.userdata = localdata;
-      this.username = JSON.parse(this.userdata)
-}
+    let localdata = localStorage.getItem('ecomdata')
+    this.userdata = localdata;
+    this.username = JSON.parse(this.userdata)
+  }
 }
